19557d2cdef4e5e0e5d524a0616e494ed8f5122b
nick stilwell <nickstilwell52@gmail.com>
2023-05-15 22:03:01 +0000

    timer that turns the number given into seconds and tells you when its done. supports unlimited arguments, each argument must be a positive number

---
+++ timer1.js
@@ -0,0 +1,21 @@
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log('please provide a timer');
+  return;
+}
+
+const timer = function([newTimer,...timers]) {
+  let remainingTimers = timers.flat();
+  if (newTimer.length > 0) {
+    if (typeof +newTimer === 'number') {
+      if (newTimer > 0) {
+        setTimeout(() => console.log('your timer is up.'),(newTimer * 1000));
+      }
+    }
+  }
+  if (remainingTimers.length > 0) {
+    timer(remainingTimers);
+  }
+};
+
+timer(args);
